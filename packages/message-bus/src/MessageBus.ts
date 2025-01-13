b2fd4573084f6d8297b4ad1269e02663c27e8276
createhb21 <alsghk9701@gmail.com>
2025-01-14 00:18:51 +0900

    feat: MFA 환경의 앱 간 이벤트 통신을 위한 MessageBus 패키지 구현

---
+++ packages/message-bus/src/MessageBus.ts
@@ -0,0 +1,25 @@
+import type { EventMap, EventName, EventHandler } from './MessageBus.types'
+
+export class MessageBus {
+  private handlers: Map<EventName, Set<EventHandler<any>>>
+
+  constructor() {
+    this.handlers = new Map()
+  }
+
+  publishEvent<T extends EventName>(eventName: T, data: EventMap[T]) {
+    const handlers = this.handlers.get(eventName)
+    handlers?.forEach(handler => handler(data))
+  }
+
+  subscribe<T extends EventName>(eventName: T, handler: EventHandler<T>) {
+    if (!this.handlers.has(eventName)) {
+      this.handlers.set(eventName, new Set())
+    }
+    this.handlers.get(eventName)?.add(handler)
+  }
+
+  unsubscribe<T extends EventName>(eventName: T, handler: EventHandler<T>) {
+    this.handlers.get(eventName)?.delete(handler)
+  }
+}
